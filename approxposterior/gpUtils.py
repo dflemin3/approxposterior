updated example notebooks, fixed sign error

approxposterior/gpUtils.py
@@ -184,12 +184,18 @@ def optimizeGP(gp, theta, y, seed=None, nRestarts=5, method=None, options=None,
         fn = util.functionWrapperArgsOnly(minimize, **mKwargs)
         results = optPool.map(fn, iterables)
 
-    # Extract solutions
+    # Extract solutions and recompute marginal log likelihood for solutions
     for result in results:
         res.append(result.x)
-        mll.append(result.fun)
 
-    # Pick result with largest log likelihood
+        # Update the kernel
+        gp.set_parameter_vector(result.x)
+        gp.recompute()
+
+        # Compute marginal log likelihood
+        mll.append(gp.log_likelihood(y, quiet=True))
+
+    # Pick result with largest marginal log likelihood
     ind = np.argmax(mll)
 
     # Update gp

approxposterior/tests/test_OptimizeGP.py
@@ -82,8 +82,9 @@ def testGPOpt():
     hypeMult = gp.get_parameter_vector()
 
     errMsg = "ERROR: GP hyperparameters are not close to the true value!"
+    errMult = "ERROR: Single and multi GP hyperparameters are not close!"
     hypeTrue = [-135.91343934, -0.68256741, 2.1394469]
-    assert np.allclose(hypeSin, hypeMult), errMsg
+    assert np.allclose(hypeSin, hypeMult), errMult
     assert np.allclose(hypeSin, hypeTrue), errMsg
     assert np.allclose(hypeMult, hypeTrue), errMsg
 # end function