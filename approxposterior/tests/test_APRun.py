f0c6b2e37759745f16d234ca3109fd07974c3b15
David Fleming <dflemin3@uw.edu>
2019-10-24 12:33:23 -0700

    messing with tests

--- approxposterior/tests/test_APRun.py
+++ approxposterior/tests/test_APRun.py
@@ -41,7 +41,7 @@ def test_run():
         y[ii] = lh.rosenbrockLnlike(theta[ii]) + lh.rosenbrockLnprior(theta[ii])
 
     # Create the the default GP which uses an ExpSquaredKernel
-    gp = gpUtils.defaultGP(theta, y, order=None, white_noise=np.log(1.0))
+    gp = gpUtils.defaultGP(theta, y, order=None, white_noise=0)
 
     # Initialize object using the Wang & Li (2017) Rosenbrock function example
     # Use default GP initialization: ExpSquaredKernel
@@ -63,8 +63,8 @@ def test_run():
     samples = ap.sampler.get_chain(discard=ap.iburns[-1], flat=True, thin=ap.ithins[-1])
     x1Med, x2Med = np.median(samples, axis=0)
 
-    diffX1 = np.fabs(0.04 - x1Med)
-    diffX2 = np.fabs(1.29 - x2Med)
+    diffX1 = np.fabs(0.0 - x1Med)
+    diffX2 = np.fabs(1.31 - x2Med)
 
     # Differences between estimated and true medians must be close-ish, but not
     # perfect because we've using a small number of samples to make this test
--- examples/example.py
+++ examples/example.py
@@ -33,7 +33,7 @@ for ii in range(len(theta)):
     y[ii] = lh.rosenbrockLnlike(theta[ii]) + lh.rosenbrockLnprior(theta[ii])
 
 # Create the the default GP which uses an ExpSquaredKernel
-gp = gpUtils.defaultGP(theta, y, order=None, white_noise=0)
+gp = gpUtils.defaultGP(theta, y, order=None, white_noise=1)
 
 # Initialize object using the Wang & Li (2017) Rosenbrock function example
 ap = approx.ApproxPosterior(theta=theta,
@@ -60,7 +60,7 @@ fig = corner.corner(samples, quantiles=[0.16, 0.5, 0.84], show_titles=True,
                     scale_hist=True, plot_contours=True)
 
 # Plot where forward model was evaluated - uncomment to plot!
-#fig.axes[2].scatter(ap.theta[:,0], ap.theta[:,1], s=10, color="red", zorder=20)
+fig.axes[2].scatter(ap.theta[:,0], ap.theta[:,1], s=10, color="red", zorder=20)
 
 # Save figure
 fig.savefig("finalPosterior.png", bbox_inches="tight")
